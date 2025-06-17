4b6348a92faefed9ad795c252590e21990f128f6
DevBuike <142910858+DevBuike@users.noreply.github.com>
2025-06-17 10:28:15 +0000

    updating [id].tsx

--- pages/property/[id].tsx
+++ pages/property/[id].tsx
@@ -10,8 +10,22 @@ export default function PropertyPage() {
   if (!property) return <p>Property not found</p>;
 
   return (
-    <div>
+    <div className="container mx-auto px-4 py-8">
+      {/* Property Detail */}
       <PropertyDetail property={property} />
+
+      {/* Responsive Grid for Booking and Reviews */}
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 mt-10">
+        {/* Booking section (takes 1/3 on large screens) */}
+        <div className="lg:col-span-1">
+          <BookingSection property={property} />
+        </div>
+
+        {/* Review section (takes 2/3 on large screens) */}
+        <div className="lg:col-span-2">
+          <ReviewSection propertyId={property.id} />
+        </div>
+      </div>
     </div>
   );
 }
